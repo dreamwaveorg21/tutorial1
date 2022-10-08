04827b9106708efd08a9a1c0d31ef6dd68a4d4ae
amar <dreamwaveorg@gmail.com>
2022-10-08 10:16:44 +0530

    First application

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,5 @@ void main() {
   // runApp(const MaterialApp(home: Text('Hello Word')));       // if we use this, in MaterialApp parent widget/layer, we add test Hello Word which start from upper-left corner.
   // runApp(const MaterialApp(home: Center(child: Text('Hello Word'))));  // if we use this, in MaterialApp parent widget/layer, we add center widget and then test Hello Word which start from upper-left corner.
   // add coma after every bracket and right click select reformat code with 'dart format'
-  runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello Word'),
-      ),
-    ),
-  );
+  runApp( const MaterialApp(home: Center(child: Text('Hello Word'))));
 }
